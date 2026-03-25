cbfe62356feaa0e8d166fd2d78b4746a8fdf8275
Rushild25 <rushild25@gmail.com>
2026-03-25 17:04:32 +0530

    fix: clean env setup, remove API keys and update .gitignore

--- test.py
+++
@@ -1,6 +0,0 @@
-from langchain_yt_dlp.youtube_loader import YoutubeLoaderDL
-loader = YoutubeLoaderDL.from_youtube_url(
-    "https://www.youtube.com/watch?v=dQw4w9WgXcQ", add_video_info=True
-)
-documents = loader.load()
-print(documents[0].metadata)
